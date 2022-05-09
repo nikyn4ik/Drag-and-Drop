244ad301b93c8eb1ab771d4d477f25acf623358e
Nika <nikyn4ik@gmail.com>
2022-05-09 16:56:49 +0300

    Create MW and View

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Text;
+
 
 namespace Test
 {
@@ -23,6 +18,47 @@ namespace Test
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LstBxImage_DragLeave(object sender, DragEventArgs e)
+        {
+            LstBxImage.Background = Brushes.WhiteSmoke;
+        }
+
+        private void LstBxImage_DragEnter(object sender, DragEventArgs e)
+        {
+            LstBxImage.Background = Brushes.SpringGreen;
+        }
+
+        public static class ImageHelper
+        {
+
+            public static string GetFileNamesFromFiles(string[] files)
+            {
+                StringBuilder allFileNames = new StringBuilder();
+
+                foreach (var file in files)
+                {
+                    allFileNames.Append(file);
+                }
+
+                return allFileNames.ToString();
+            }
+        }
+
+        public static bool FileImg(string path)
+        {
+            List<string> imageExtensions = new List<string> { ".jpg", ".jpeg", ".jfif", ".png", ".jpe" };
+
+            for (int i = 0; i < imageExtensions.Count; i++)
+            {
+                if (path.ToLower().EndsWith(imageExtensions[i].ToLower()))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
---
+++ Test/View.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        public List<Image> Img { get; set; }
+
+        public View(List<Image> img, int index)
+        {
+            InitializeComponent();
+            Img = img;
+            ImgFromListBox.Source = Img[index].Source;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var NW = new MainWindow();
+            this.Hide();
+            NW.Show();
+        }
+    }
+}
